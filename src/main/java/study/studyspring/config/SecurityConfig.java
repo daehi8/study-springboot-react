652d60bfa3ef328696fe4ec6b57909520cfd4cb6
Yoon Dae Hee <daehi8@naver.com>
2023-04-08 14:34:39 +0900

    fix SecurityConfig

--- src/main/java/study/studyspring/config/SecurityConfig.java
+++ src/main/java/study/studyspring/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package study.studyspring.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +16,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // enable h2-console
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
                 .authorizeHttpRequests()
-                .antMatchers("/api/test").permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/hello")).permitAll()
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
                 .anyRequest().authenticated();
 
         return http.build();
