affbdfdc6786c8f3eb09f09760312a4d9888695a
Yoon Dae Hee <daehi8@naver.com>
2023-04-21 20:10:32 +0900

    first Setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <div>"hello"</div>;
+  return <div>Hello</div>;
 }
 
 export default App;
