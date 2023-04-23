c4acfcfb708320abc19034a715c812e2eec30b5b
Yoon Dae Hee <daehi8@naver.com>
2023-04-23 12:16:40 +0900

    Add BookController

--- src/main/java/study/studyspring/controller/BookController.java
+++ src/main/java/study/studyspring/controller/BookController.java
@@ -1,13 +1,40 @@
 package study.studyspring.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import study.studyspring.entity.Book;
+import study.studyspring.service.BookService;
 
+@RequiredArgsConstructor
 @RestController
 public class BookController {
 
+    private final BookService bookServie;
+
+    @PostMapping("/book")
+    public ResponseEntity<?> save(@RequestBody Book book){
+        return new ResponseEntity<>(bookServie.saveBook(book), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/book")
     public ResponseEntity<?> findALl(){
-        return new ResponseEntity<String>("ok", HttpStatus.OK);
+        return new ResponseEntity<>(bookServie.getAllBook(), HttpStatus.OK);
+    }
+
+    @GetMapping("/book/{id}")
+    public ResponseEntity<?> findbyId(@PathVariable Long id){
+        return new ResponseEntity<>(bookServie.getOneBook(id), HttpStatus.OK);
+    }
+
+    @PutMapping("book/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Book book){
+        return new ResponseEntity<>(bookServie.modifyBook(id, book), HttpStatus.OK);
+    }
+
+    @DeleteMapping("book/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id, @RequestBody Book book){
+        return new ResponseEntity<>(bookServie.deleteBook(id), HttpStatus.OK);
     }
 }
