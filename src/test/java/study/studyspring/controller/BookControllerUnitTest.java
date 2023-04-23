5b0e6576cd826ac7beed4df01eb1630b7c8878ff
Yoon Dae Hee <daehi8@naver.com>
2023-04-23 19:41:03 +0900

    Add BookTest

--- src/test/java/study/studyspring/controller/BookControllerUnitTest.java
+++ src/test/java/study/studyspring/controller/BookControllerUnitTest.java
@@ -1,8 +1,77 @@
 package study.studyspring.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import study.studyspring.entity.Book;
+import study.studyspring.jwt.TokenProvider;
+import study.studyspring.service.BookService;
+import study.studyspring.service.UserService;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
 @WebMvcTest
 public class BookControllerUnitTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @MockBean
+    private BookService bookService;
+
+    @MockBean
+    private TokenProvider tokenProvider;
+
+    @MockBean
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    public void saveTest() throws Exception {
+        // given 테스트 준비
+        Book book = new Book(null, "TestTitle", "TestAuthor");
+        String content = new ObjectMapper().writeValueAsString(book);
+        when(bookService.saveBook(book)).thenReturn(new Book(1L, "TestTitle", "TestAuthor"));
+
+        // when 테스트 실행
+        ResultActions resultActions = mockMvc.perform(post("/book").with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then 검증
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("TestTitle"))
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
