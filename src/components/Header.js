96c4188f401705e2adb56ccb84c650848121e940
Yoon Dae Hee <daehi8@naver.com>
2023-05-13 18:10:46 +0900

    Add Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,33 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import React from "react";
+import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-function ColorSchemesExample() {
+const Header = () => {
   return (
     <>
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Link to="/" className="navbar-brand">
-            홈
+      <Navbar bg="dark" variant="dark">
+        <Link to="/" className="navbar-brand">
+          홈
+        </Link>
+        <Nav className="mr-auto">
+          <Link to="/joinForm" className="nav-link">
+            회원가입
           </Link>
-          <Nav className="me-auto">
-            <Link to="/joinForm" className="nav-link">
-              회원가입
-            </Link>
-            <Link to="/loginForm" className="nav-link">
-              로그인
-            </Link>
-            <Link to="/saveForm" className="nav-link">
-              글쓰기
-            </Link>
-          </Nav>
-        </Container>
+          <Link to="/loginForm" className="nav-link">
+            로그인
+          </Link>
+          <Link to="/saveForm" className="nav-link">
+            글쓰기
+          </Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
       </Navbar>
+      <br />
     </>
   );
-}
+};
 
-export default ColorSchemesExample;
+export default Header;
